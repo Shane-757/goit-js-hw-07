1fcb7664af07ab1856198d5b9232c4d7d35d66a7
Shane-757 <underwoodshane@ymail.com>
2023-02-13 16:49:25 -0500

    update

--- scr-7/js/01-gallery.js
+++ scr-7/js/01-gallery.js
@@ -1,42 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const imageList = document.querySelector('.gallery');
 
-let imageGallery = "";
-
-const images = galleryItems.map((element) => 
- imageGallery += 
-  `<div class="gallery__item">
-
-  <a class="gallery__link" href="${element.original}">
-    <img
-      class="gallery__image"
-      src="${element.preview}"
-      data-source="${element.original}"
-      alt="${element.description}"
-    />
-  </a>
-</div>`).join("");
-
+let imageGallery = creatImagesMarkup();
 
 imageList.insertAdjacentHTML("afterbegin", imageGallery);
-const img = document.querySelector('.gallery__image');
-console.log(img)
-imageList.addEventListener("click", modal);
 
+imageList.addEventListener("click", openModal);
 
-function modal(event) {
+function creatImagesMarkup() {
+  return galleryItems
+    .map(
+      element => 
+          `<div class="gallery__item">
+              <a class="gallery__link" href="${element.original}">
+                <img
+                  class="gallery__image"
+                  src="${element.preview}"
+                  data-source="${element.original}"
+                  alt="${element.description}"
+                />
+              </a>
+            </div>`
+         )
+    .join("");
+}
+
+function openModal(event) {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return
   }
   const largeImage = event.target.dataset.source;
     
-  const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`)
-  console.log(event.target.dataset.source)
+  const instance = basicLightbox.create(`<img src="${largeImage}">`)
 
   instance.show();
 }  
--- scr-7/js/02-lightbox.js
+++ scr-7/js/02-lightbox.js
@@ -1,41 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const imageList = document.querySelector('.gallery');
 
-let imageGallery = "";
-
-const images = galleryItems.map((element) => 
- imageGallery += 
-  
- 
-
-  `<a class="gallery__link" href="${element.original}">
-    <img
-      class="gallery__image"
-      src="${element.preview}"
-      
-      alt="${element.description}"
-    />
-  </a>
-`).join("");
-
+let imageGallery = creatImagesMarkup();
 
 imageList.insertAdjacentHTML("afterbegin", imageGallery);
-const img = document.querySelector('.gallery__image');
-console.log(img)
+
 imageList.addEventListener("click", modal);
 
+function creatImagesMarkup() {
+  return galleryItems
+  .map(
+    element => 
+        `<a class="gallery__link" href="${element.original}">
+            <img
+              class="gallery__image"
+              src="${element.preview}"
+              alt="${element.description}"
+            />
+          </a>`
+          )
+          .join("");
+}
 
 function modal(event) {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return
   }
-  const largeImage = event.target.dataset.source;
-    
   var lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay: 250, captionPosition: "bottom",});
     
   lightbox.on()
